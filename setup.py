59ffbcbf9e0811a77ebd3c3d33b1dc31c7686b7d
Haruhiko Kawai <mertris@outlook.jp>
2025-01-06 01:18:16 +0900

    rename talker

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'battery = mypkg.talker:main',
+            'battery = mypkg.battery:main',
         ],
     },
 )
