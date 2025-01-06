9065d39fe846359551e4126406f0ae00c1ae8b61
Haruhiko Kawai <mertris@outlook.jp>
2025-01-06 12:39:14 +0900

    fix test

--- test/test.bash
+++ test/test.bash
@@ -7,11 +7,15 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-ros2 run mypkg battery & pub=$!
+ros2 run mypkg battery &
+PUB_PID=$!
+
 sleep 5
 
-timeout 60 ros2 topic echo /batterycheck > /tmp/mypkg.log 
+timeout 60 ros2 topic echo /batterycheck > /tmp/mypkg.log &
+ECHO_PID=$!
 
-kill $pub
+kill $PUB_PID
+kill $ECHO_PID
 
 cat /tmp/mypkg.log | grep 'data: 99'
